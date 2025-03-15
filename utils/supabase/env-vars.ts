d0095d68379d0e3c31f1640ac6cf9c7f3cba0edf
Luke MacKenzie <mackenzieluke94@gmail.com>
2025-03-15 19:18:06 -0400

    fix

--- utils/supabase/env-vars.ts
+++ utils/supabase/env-vars.ts
@@ -1,7 +1,4 @@
 export const SUPABASE_CONFIG = {
-  url:
-    process.env.APP_ENV === 'production'
-      ? process.env.NEXT_PUBLIC_SUPABASE_URL
-      : process.env.NEXT_PUBLIC_SUPABASE_URL_DEV,
+  url: process.env.NEXT_PUBLIC_SUPABASE_URL,
   anonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 };
